df95308d194d4d1f6b132f804c39b96fda267581
RonnieJ507 <RonnieJ507@gmail.com>
2025-06-11 19:14:50 -0500

    Getting Previous Issues in the Comic Series

--- main.py
+++ main.py
@@ -20,33 +20,67 @@ def get_marvel_auth():
     }
 
 def get_comic_by_upc(upc):
-    base_url = "https://gateway.marvel.com/v1/public/comics"
+    url = "https://gateway.marvel.com/v1/public/comics"
     params = {
         'upc': upc,
         **get_marvel_auth()
     }
 
-    response = requests.get(base_url, params=params)
+    response = requests.get(url, params=params)
     if response.status_code != 200:
         raise Exception(f"Marvel API error: {response.status_code}")
     
     data = response.json()
     return data["data"]["results"][0] if data else None
 
+def get_previous_issues(comic):
+    series_url = comic["series"]["resourceURI"]
+    curr_issue = comic["issueNumber"]
+
+    url = series_url + "/comics"
+    params = {
+        "orderBy": "issueNumber",
+        "limit": 100,
+        "noVariants": "true",
+        **get_marvel_auth()
+    }
+
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Marvel API error: {response.status_code}")
+    
+    data = response.json()
+
+    if data:
+        data = data["data"]["results"]
+
+        #Get the previous issues and no variants
+        previous = [c for c in data if c["issueNumber"] < curr_issue]
+        return previous
+
+    return None
+
 def main():
     upc = "75960620570700311" # Example: Aliens VS. Avengers (2024) #3
     comic = get_comic_by_upc(upc)
 
     if comic:
+        summaries = []
         print(f'''Marvel Comics AI Recapper
-    Marvel API Public Key: {MARVEL_PUBLIC}
-    Marvel API Private Key: {MARVEL_PRIVATE}
     Title: {comic["title"]}
-    Description: {comic["description"]}
     Stories: {comic["stories"]}
     Events: {comic["events"]}''')
+
+        previous_issues = get_previous_issues(comic)
+        
+        print("Previous Issue Titles")
+        for c in previous_issues:
+            summaries.append(c["description"])
+            print(c["title"])
+
     else:
         print("No comic found for this UPC.")
+        return
 
 
 if __name__ == "__main__":
